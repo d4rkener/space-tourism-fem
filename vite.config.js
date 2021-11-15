7fa2a198617874012cc5fef1b26d201155bb3d8d
Rahul Shaw <shawrahul2006@gmail.com>
2021-11-15 15:55:16 +0530

    ADD: Desktop Navbar Working

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { defineConfig } = require('vite');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { ViteAliases } = require('vite-aliases');
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  plugins: [ViteAliases()],
+});
