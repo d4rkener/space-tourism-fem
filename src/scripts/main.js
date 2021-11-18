ee0e430b5112d10f2f8e79c5033613d8d609dfdd
Rahul Shaw <shawrahul2006@gmail.com>
2021-11-18 15:35:09 +0530

    ADD: Mobile Navigation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 import '../scss/style.scss';
 
-console.log('Hello, World');
+// Variables
+const hamburgerOpenBtn = document.getElementById('hamburgerOpenBtn');
+const mobileNavbar = document.getElementById('mobileNavbar');
+const hamburgerCloseBtn = document.getElementById('hamburgerCloseBtn');
+
+// Functions
+const openMobileNavbar = () => {
+  hamburgerOpenBtn.classList.add('hide');
+  mobileNavbar.classList.remove('hide');
+};
+
+const closeMobileNavbar = () => {
+  mobileNavbar.classList.add('hide');
+  hamburgerOpenBtn.classList.remove('hide');
+};
+
+// Event Listeners
+hamburgerOpenBtn.addEventListener('click', openMobileNavbar);
+hamburgerCloseBtn.addEventListener('click', closeMobileNavbar);
